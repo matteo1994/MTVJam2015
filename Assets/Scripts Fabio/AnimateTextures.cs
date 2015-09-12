00ece783635b4da5d2f411c903404df499e27e75
Fabio Ferrara <fabioferrara1989@gmail.com>
2015-09-12 12:03:47 +0200

    Animations e models nuovi

---
+++ Assets/Scripts Fabio/AnimateTextures.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AnimateTextures : MonoBehaviour {
+	
+	public float FramesPerSecond;
+	public Texture[] artex;
+	int frame = 0;
+	private float SecondsPerFrame;
+	float timer = 0.0f;
+	
+	void Start() { SecondsPerFrame = 1.0f / FramesPerSecond; }
+	
+	void Update () {
+		
+		if (timer >= SecondsPerFrame) {
+			int iT = Mathf.FloorToInt(timer / SecondsPerFrame);
+			timer -= SecondsPerFrame * iT; 
+			frame = (frame + iT) % artex.Length;
+			GetComponent<Renderer>().material.mainTexture = artex[frame];
+		}
+		timer += Time.deltaTime;
+	}
+}
