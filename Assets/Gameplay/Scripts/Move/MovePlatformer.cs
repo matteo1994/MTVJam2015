e797189de658ca5f3ec6038641e1a80d3bdb5f8b
Pier Luca Lanzi <pierluca.lanzi@gmail.com>
2015-09-12 12:20:36 +0200

    no message

--- Assets/Gameplay/Scripts/Move/MovePlatformer.cs
+++ Assets/Gameplay/Scripts/Move/MovePlatformer.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class MovePlatformer : MoveComponent {
 
+
+	PlatformerChar platformerChar;
+
+	float y0 = 0f;
+	float height = 1.5f; 
+	float offset = 1f;
+
 	class Cinematic {
 		public float x;
 		public float y;
@@ -15,32 +22,75 @@ public class MovePlatformer : MoveComponent {
 
 	public bool onPlatform = true;
 	public bool isJumping = false;
+	private float speed = 10f;
 
-	public float gravity = 9.8f*2.5f;
+	private float gravity = 50f;
+
 
 	Cinematic current_speed = new Cinematic(0f,0f);
-	Cinematic acceleration = new Cinematic(10f,0f);
+	Cinematic acceleration = new Cinematic(10f,20f);
 
-	public float max_speed = 20f;
+	void Awake() {
+		y0 = transform.position.y;
+		Debug.LogError ("Y0=" + y0);
+
+		platformerChar = GetComponent<PlatformerChar> ();
+
+	}
 
     public override void Move(InputParams _input)
     {
+		float dx = 0;
+
 		if (!onPlatform) {
-
-		}
-
-		if (_input.fire) {
-			Debug.LogError("JUMP");
-			//current_speed = current_speed + acceleration * Time.deltaTime;
-			//current_speed = Mathf.Min (current_speed, max_speed);
+			current_speed.y = current_speed.y + acceleration.y * Time.deltaTime - gravity*Time.deltaTime;
+			transform.position = transform.position + Time.deltaTime*(transform.up*current_speed.y+transform.right*current_speed.x);
 		}
 
 		if (onPlatform) {
-			if (_input.x != 0) {
-				current_speed.x = current_speed.x + Mathf.Sign(_input.x) * acceleration.x * Time.deltaTime;
-				//current_speed.x = Mathf.Min (current_speed, max_speed);
-			}
+			if (!isJumping && _input.fire) {
+
+				isJumping = true;
+				onPlatform = false;
+
+				current_speed.y = 15f + current_speed.y + acceleration.y * Time.deltaTime - gravity*Time.deltaTime;
+
+			} 
+
+			if (_input.x!=0)
+				current_speed.x = Mathf.Sign (_input.x)*speed;
+			else current_speed.x = 0;
+
+			transform.position = transform.position + Time.deltaTime*(transform.up*current_speed.y+transform.right*current_speed.x);
+
+
 		}
     }
 
+	void Update() {
+		if (transform.position.y < 5.5f) {
+			Debug.LogError("I AM DYING!");
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.LogError ("COLLISION!");
+		if (other.gameObject.tag == "Platform") {
+			Vector3 d = transform.position - other.gameObject.transform.position;
+
+			Debug.LogError("D = "+d);
+			if (d.y<offset) return; 
+
+			current_speed.y = 0f;
+			transform.position = new Vector3(transform.position.x,other.gameObject.transform.position.y+height, transform.position.z);
+			onPlatform = true;
+			isJumping = false;
+			Debug.LogError("LANDED! YO!");
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		onPlatform = false;
+	}
 }
---
+++ Assets/Gameplay/Scripts/Move/PlatformGeneration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGeneration : MonoBehaviour {
+
+	float creation_after = 4f;
+	float destruction_after = 3f; 
+	float upper_limit = 15f;
+	float lower_limit = -15f;
+
+
+	public GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GeneratePlatforms ());
+	}
+
+	IEnumerator GeneratePlatforms() {
+		yield return new WaitForSeconds (creation_after);
+
+//		float dx = Random.Range ();
+//		Vector3 pp1 = new Vector3
+//		Instantiate
+
+		yield return null;
+	}
+}
